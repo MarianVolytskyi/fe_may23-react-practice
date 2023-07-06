08e1e4b2e1c3090b1bf04dafb1f3ed6df061edad
Marian Volytskyi <volmark00@gmail.com>
2023-07-06 11:45:33 +0300

    task 3 and 4 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import cn from "classnames";
-import React, { useState } from "react";
-import "./App.scss";
+import cn from 'classnames';
+import React, { useState } from 'react';
+import './App.scss';
 
-import usersFromServer from "./api/users";
-import categoriesFromServer from "./api/categories";
-import productsFromServer from "./api/products";
+import usersFromServer from './api/users';
+import categoriesFromServer from './api/categories';
+import productsFromServer from './api/products';
 
 const products = productsFromServer.map((product) => {
   const category = categoriesFromServer.find(
-    (categoryFind) => categoryFind.id === product.categoryId
+    categoryFind => categoryFind.id === product.categoryId,
   );
-  const user = usersFromServer.find((owner) => category.ownerId === owner.id);
+  const user = usersFromServer.find(owner => category.ownerId === owner.id);
 
   return { ...product, category, user };
 });
 
-console.log(products);
-function getFilteredProduct(products, filter) {
-  let filteredProducts = [...products];
+// console.log(products);
 
-  // if (query) {
-  //   const normalizedQuery = query.toLowerCase();
+function getFilteredProduct(productsForfilter, filter, query) {
+  let filteredProducts = [...productsForfilter];
 
-  //   filteredPeople = filteredPeople.filter(
-  //     person => person.name.toLowerCase().startsWith(normalizedQuery),
-  //   );
-  // }
+  if (query) {
+    const normalizedQuery = query.toLowerCase();
 
-  if (filter !== "all") {
     filteredProducts = filteredProducts.filter(
-      (product) => product.user.name === filter
+      product => product.name.toLowerCase().includes(normalizedQuery),
+    );
+  }
+
+  if (filter !== 'all') {
+    filteredProducts = filteredProducts.filter(
+      product => product.user.name === filter,
     );
   }
 
@@ -37,9 +38,10 @@ function getFilteredProduct(products, filter) {
 }
 
 export const App = () => {
-  const [filterName, setFilterName] = useState("all");
+  const [filterName, setFilterName] = useState('all');
+  const [query, setQuery] = useState('');
 
-  const visibleProducts = getFilteredProduct(products, filterName);
+  const visibleProducts = getFilteredProduct(products, filterName, query);
 
   return (
     <div className="section">
@@ -55,9 +57,9 @@ export const App = () => {
                 data-cy="FilterAllUsers"
                 href="#/"
                 onClick={() => {
-                  setFilterName("all");
+                  setFilterName('all');
                 }}
-                className={cn({ "is-active": filterName === "all" })}
+                className={cn({ 'is-active': filterName === 'all' })}
               >
                 All
               </a>
@@ -66,9 +68,9 @@ export const App = () => {
                 data-cy="FilterUser"
                 href="#/"
                 onClick={() => {
-                  setFilterName("Roma");
+                  setFilterName('Roma');
                 }}
-                className={cn({ "is-active": filterName === "Roma" })}
+                className={cn({ 'is-active': filterName === 'Roma' })}
               >
                 Roma
               </a>
@@ -76,9 +78,9 @@ export const App = () => {
               <a
                 data-cy="FilterUser"
                 href="#/"
-                className={cn({ "is-active": filterName === "Anna" })}
+                className={cn({ 'is-active': filterName === 'Anna' })}
                 onClick={() => {
-                  setFilterName("Anna");
+                  setFilterName('Anna');
                 }}
               >
                 Anna
@@ -88,9 +90,9 @@ export const App = () => {
                 data-cy="FilterUser"
                 href="#/"
                 onClick={() => {
-                  setFilterName("Max");
+                  setFilterName('Max');
                 }}
-                className={cn({ "is-active": filterName === "Max" })}
+                className={cn({ 'is-active': filterName === 'Max' })}
               >
                 Max
               </a>
@@ -99,9 +101,9 @@ export const App = () => {
                 data-cy="FilterUser"
                 href="#/"
                 onClick={() => {
-                  setFilterName("John");
+                  setFilterName('John');
                 }}
-                className={cn({ "is-active": filterName === "John" })}
+                className={cn({ 'is-active': filterName === 'John' })}
               >
                 John
               </a>
@@ -113,22 +115,27 @@ export const App = () => {
                   data-cy="SearchField"
                   type="text"
                   className="input"
+                  value={query}
+                  onChange={event => setQuery(event.target.value)}
                   placeholder="Search"
-                  value="qwe"
                 />
 
                 <span className="icon is-left">
                   <i className="fas fa-search" aria-hidden="true" />
                 </span>
 
+                { query && (
                 <span className="icon is-right">
                   {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
                   <button
                     data-cy="ClearButton"
                     type="button"
                     className="delete"
+                    onClick={() => setQuery('')}
                   />
                 </span>
+                )
+                }
               </p>
             </div>
 
@@ -170,6 +177,10 @@ export const App = () => {
                 data-cy="ResetAllButton"
                 href="#/"
                 className="button is-link is-outlined is-fullwidth"
+                onClick={()=>{
+                  setFilterName('all');
+                  setQuery('');
+                }}
               >
                 Reset all filters
               </a>
@@ -237,7 +248,7 @@ export const App = () => {
               </thead>
 
               <tbody>
-                {visibleProducts.map((product) => (
+                {visibleProducts.map(product => (
                   <tr data-cy="Product" key={product.id}>
                     <td className="has-text-weight-bold" data-cy="ProductId">
                       {product.id}
@@ -251,8 +262,8 @@ export const App = () => {
                     <td
                       data-cy="ProductUser"
                       className={cn({
-                        "has-text-link": product.user.sex === "m",
-                        "has-text-danger": product.user.sex === "f",
+                        'has-text-link': product.user.sex === 'm',
+                        'has-text-danger': product.user.sex === 'f',
                       })}
                     >
                       {product.user.name}
